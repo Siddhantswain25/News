7e2f17edfbf2bf42b09ac4dd2d985bea2365d325
Naresh Chandra Swain <nareshchandraswain@Nareshs-MacBook-Air.local>
2018-03-31 16:24:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nareshchandraswain/news/ArticleLoader.java
@@ -0,0 +1,38 @@
+package com.example.nareshchandraswain.news;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import java.util.List;
+
+class ArticleLoader extends AsyncTaskLoader {
+
+    private final Bundle pBundle;
+
+    // Article loader
+    public ArticleLoader(final Context context, final Bundle bundle) {
+        super(context);
+        pBundle = bundle;
+    }
+
+    // Start loading article
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    @Nullable
+    public Object loadInBackground() {
+        // Create base article list
+        List<BaseArticleData> articleList = null;
+        // If bundle has data get articles
+        if (pBundle != null) {
+            articleList = QueryUtils.fetchArticleData(pBundle.getString("uri"));
+        }
+        // Return articles list
+        return articleList;
+    }
+}
---
+++ app/src/main/java/com/example/nareshchandraswain/news/DatePickerFragment.java
@@ -0,0 +1,137 @@
+package com.example.nareshchandraswain.news;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    // Date type to check if it is fromDate or toDate
+    private int dateType = 0;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Get current date type
+        dateType = getArguments().getInt("dateType");
+        // Get current date
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        // Show date picker dialog
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        // Send back date to main activity
+        switch (dateType) {
+            case 1:
+                setFromDate(year, month, day);
+                break;
+            case 2:
+                setToDate(year, month, day);
+                break;
+        }
+    }
+
+    private void setFromDate(int year, int month, int day) {
+
+        // Set year
+        String sYear = String.valueOf(year);
+        // Set month
+        String sMonth;
+        month += 1;
+        if (month < 10) {
+            sMonth = "0" + String.valueOf(month);
+        } else {
+            sMonth = String.valueOf(month);
+        }
+        // Set day
+        String sDay;
+        if (day < 10) {
+            sDay = "0" + String.valueOf(day);
+        } else {
+            sDay = String.valueOf(day);
+        }
+        // Construct string
+        String selectedDate = sYear + "-" + sMonth + "-" + sDay;
+        // Convert dates to milliseconds
+        String stringTo = MainActivity.TO_DATE;
+        Date dateFrom = null;
+        try {
+            dateFrom = new SimpleDateFormat("yyyy-MM-dd").parse(selectedDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date dateTo = null;
+        try {
+            dateTo = new SimpleDateFormat("yyyy-MM-dd").parse(stringTo);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long millisFrom = dateFrom != null ? dateFrom.getTime() : 0;
+        long millisTo = dateTo != null ? dateTo.getTime() : 0;
+        // Check date valid from is before to date and neither after today
+        if ((System.currentTimeMillis() > millisTo) && (millisFrom <= millisTo)) {
+            // Set date to button
+            Button fromDate = (Button) getActivity().findViewById(R.id.from_date_button);
+            fromDate.setText(selectedDate);
+            MainActivity.FROM_DATE = selectedDate;
+        }
+    }
+
+    private void setToDate(int year, int month, int day) {
+
+        // Set year
+        String sYear = String.valueOf(year);
+        // Set month
+        String sMonth;
+        month += 1;
+        if (month < 10) {
+            sMonth = "0" + String.valueOf(month);
+        } else {
+            sMonth = String.valueOf(month);
+        }
+        // Set day
+        String sDay;
+        if (day < 10) {
+            sDay = "0" + String.valueOf(day);
+        } else {
+            sDay = String.valueOf(day);
+        }
+        // Construct string
+        String selectedDate = sYear + "-" + sMonth + "-" + sDay;
+        String stringFrom = MainActivity.FROM_DATE;
+        Date dateFrom = null;
+        try {
+            dateFrom = new SimpleDateFormat("yyyy-MM-dd").parse(stringFrom);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date dateTo = null;
+        try {
+            dateTo = new SimpleDateFormat("yyyy-MM-dd").parse(selectedDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long millisFrom = dateFrom != null ? dateFrom.getTime() : 0;
+        long millisTo = dateTo != null ? dateTo.getTime() : 0;
+        // Check date valid from is before to date and neither after today
+        if ((System.currentTimeMillis() > millisTo) && (millisFrom <= millisTo)) {
+            // Set date to button
+            Button toDate = (Button) getActivity().findViewById(R.id.to_date_button);
+            toDate.setText(selectedDate);
+            MainActivity.TO_DATE = selectedDate;
+        }
+    }
+}
